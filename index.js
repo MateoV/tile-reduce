4efe6259f9a08579c17999d0ba2e79fb61d345e9
MateoV <matt@mapbox.com>
2015-08-17 16:10:41 -0700

    request HEAD from random # of tiles

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var fork = require('child_process').fork;
 var cpus = require('os').cpus().length;
 var rateLimit = require('function-rate-limit');
 
+var tileNum = 1000;
+
 module.exports = function (coverArea, opts){
   var workers = [];
   var tilesCompleted = 0;
@@ -27,7 +29,7 @@ module.exports = function (coverArea, opts){
       tilesCompleted++;
 
       // if all tiles have been processed, kill workers and emit 'end' event
-      if(tilesCompleted >= tiles.length){
+      if(tilesCompleted >= tileNum){
         while (workers.length) {
           workers.shift().kill('SIGHUP');
         }
@@ -55,9 +57,11 @@ function sendTiles (tiles, workers, opts) {
       opts: opts
     });
   });
-  tiles.forEach(function(tile){
-    sendTile(tile);
-  });
+  for (i = 0; i < tileNum; i++) {
+    ridx = getRandomInt(0, tiles.length-1);
+    sendTile(tiles[ridx]);
+    tiles.splice(ridx, 1);
+  }
 }
 
 function computeCover (coverArea, zoom) {
--- worker.js
+++ worker.js
@@ -13,14 +13,9 @@ process.on('message', function(data) {
       q.defer(getVectorTile, tile, tileLayer);
     });
     q.awaitAll(function(err, res){
-      if(res){
-        res.forEach(function(item){
-          item.layers.forEach(function(layer){
-            if(!layerCollection[item.name]) layerCollection[item.name] = {};
-            layerCollection[item.name][layer] = item[layer];
-          });
-        });
-        mapOperation(layerCollection, tile, function(err, message){
+      if(res[0]){
+        mapOperation(tile, function(err, message){
+          //console.log(message);
           process.send(message);
         });
       } else {
@@ -36,6 +31,8 @@ function getVectorTile(tile, tileLayer, done){
     layers:tileLayer.layers
   };
 
+  var xTile;
+
   var url = tileLayer.url.split('{x}').join(tile[0]);
   url = url.split('{y}').join(tile[1]);
   url = url.split('{z}').join(tile[2]);
@@ -43,28 +40,18 @@ function getVectorTile(tile, tileLayer, done){
   var requestOpts = {
     url: url,
     gzip: true,
-    encoding: null
+    encoding: null,
+    method: 'HEAD'
   };
   request(requestOpts, function(err, res, body) {
-    var vt;
-    try {
-      vt = new VectorTile(new Pbf(new Uint8Array(body)));
-    } catch(e){
-      done(e, null);
-    }
-    tileLayer.layers.forEach(function(layer){
-      layers[layer] = turf.featurecollection([]);
-      if(vt && vt.layers[layer]){
-        for(var i = 0; i < vt.layers[layer].length; i++){
-          try {
-            layers[layer].features.push(vt.layers[layer].feature(i).toGeoJSON(tile[0],tile[1],tile[2]));
-          } catch(e){
-            done(e, null);
-          }
-        }
+    //console.log(res.statusCode);
+    if (res.statusCode == 200) {
+      //console.log(parseInt(res.headers['content-length']));
+      if (parseInt(res.headers['content-length']) == 1882) {
+        xTile = tile;
       }
-    });
+    }
 
-    done(null, layers);
+    done(null, xTile);
   });
 }
